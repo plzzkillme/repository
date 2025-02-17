84b0cc8ee7624e33fc9eda8e0f9c7152c4f41a07
John Doe <johndoe@example.com>
2025-02-17 19:45:08 +0800

    added def multiplication

--- repository.py
+++ repository.py
@@ -2,3 +2,6 @@ print("github fucking sucks")
 print("github fucking sucks")
 print("repository")
 print("repository")
+def multiplication(multiplication):
+    print(multiplication*multiplication)
+multiplication(100)
